6c2e453e3de19d24744aa0710e4c248375160aa4
Hugo Aguirre <bul.ikana@gmail.com>
2016-10-22 16:37:05 -0700

    Mock for result cards in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+//Custom components
+import { ResultCard } from "../components/result-card/result-card"
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ResultCard
   ],
   imports: [
     IonicModule.forRoot(MyApp)
---
+++ src/components/result-card/result-card.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'result-card',
+  templateUrl: 'result-card.html'
+})
+export class ResultCard {
+
+  concept: string;
+  amount: number;
+
+  constructor() {
+    this.concept = 'Dinero disponible:';
+    this.amount = 500;
+  }
+
+}
